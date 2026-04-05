3770e3d8c1050ee5d80969cfe483e304365fa860
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-04-05 07:42:32 +0530

    feat: update package.json exports and enhance tsconfig for better type declarations

--- template/src/root.tsx
+++ template/src/root.tsx
@@ -9,6 +9,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />;
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
